e348692062d0b9939c20841dea58539c02c2372e
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-15 00:16:36 +0800

    environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,23 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
-
+  Rails.application.configure do
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_caching = false
+    config.action_mailer.default_url_options = { host: 'instacram-alpha.herokuapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: Rails.application.credentials.gmail[:email],
+      password: Rails.application.credentials.gmail[:password], 
+      domain: "smtp.gmail.com",
+      openssl_verify_mode: "none",
+    }
+end
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
