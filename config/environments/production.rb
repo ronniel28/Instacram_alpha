954c9c72734d269ffd250970380954ccb786c99e
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-15 11:18:10 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "instacram-alpha.herokuapp.com" }
   config.action_mailer.smtp_settings = {
-    user_name: Rails.application.credentials.gmail[:username],
+    user_name: Rails.application.credentials.gmail[:email],
     password: Rails.application.credentials.gmail[:password],
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
