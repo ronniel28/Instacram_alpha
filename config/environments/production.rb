c2bfb6a8be0c344e06ce330538a313ee465fbca0
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-15 10:50:41 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,8 @@ Rails.application.configure do
   config.require_master_key = true
   config.action_mailer.default_url_options = { :host=> 'https://instacram-alpha.herokuapp.com' }
   Rails.application.configure do
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_caching = false
-    config.action_mailer.default_url_options = { :host=> 'instacram-alpha.herokuapp.com', :protocol => 'https' }
-    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { :host=> 'instacram-alpha.herokuapp.com', :protocol => 'http' }
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
